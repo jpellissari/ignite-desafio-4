1078a4078fd692b4c73e104f1aaac5d69b5a0ad2
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-03-20 17:34:48 -0300

    finished challenge 4 of ignite

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,13 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user || !user.admin) throw new Error("not allowed");
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
